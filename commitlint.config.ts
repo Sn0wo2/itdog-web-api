9ea44fc155ca44693b137ce51430f8cfd4f980f3
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-09-05 07:26:45 +0800

    chore(commitlint): init config
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

---
+++ commitlint.config.ts
@@ -0,0 +1,29 @@
+import type {UserConfig} from '@commitlint/types'
+
+const config: UserConfig = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'docs',
+                'style',
+                'refactor',
+                'test',
+                'chore',
+                'revert',
+                'build',
+                'ci',
+                'perf',
+                'deps',
+            ],
+        ],
+        'subject-full-stop': [2, 'never', '.'],
+        'subject-case': [2, 'always', 'lower-case'],
+    },
+}
+
+export default config
