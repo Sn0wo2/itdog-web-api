4b4bf772e5a9fc014ee86f51757d25d4470aadf0
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-09-16 23:33:30 +0800

    fix(index): update
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 export {Client} from './Client.js';
 
-export {PingAPI} from './api/endpoint/PingAPI.js';
-export {TCPingAPI} from './api/endpoint/TCPingAPI.js';
-export {HttpAPI} from './api/endpoint/HttpAPI.js';
-export {BatchTCPingAPI} from './api/endpoint/BatchTCPingAPI.js';
-export {GenericAPI} from './api/GenericAPI.js';
-
 export * from './types.js';
 
 export {
